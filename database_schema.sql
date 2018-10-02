1abfdef2ac6925c5c292801ee08f300781799131
John Hill <pferreira@xscoating.com>
2018-10-02 03:30:12 -0500

    new tables

--- database_schema.sql
+++ database_schema.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.6.40, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 5.6.41, for macos10.13 (x86_64)
 --
 -- Host: localhost    Database: service
 -- ------------------------------------------------------
--- Server version	5.6.40
+-- Server version	5.6.41
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -45,7 +45,24 @@ CREATE TABLE `devices` (
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`device_id`),
   UNIQUE KEY `uuid` (`uuid`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `event_log`
+--
+
+DROP TABLE IF EXISTS `event_log`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `event_log` (
+  `event_id` int(11) NOT NULL AUTO_INCREMENT,
+  `event_type_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `json_metadata` text,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`event_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -62,7 +79,119 @@ CREATE TABLE `frames` (
   `viewed` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `metadata` text,
   PRIMARY KEY (`frame_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `glosspoints`
+--
+
+DROP TABLE IF EXISTS `glosspoints`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `glosspoints` (
+  `serial` int(11) NOT NULL AUTO_INCREMENT,
+  `eth_address` char(42) DEFAULT NULL,
+  `owner_id` int(11) DEFAULT NULL,
+  `issued` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`serial`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `orders` (
+  `order_id` int(11) NOT NULL AUTO_INCREMENT,
+  `shipping_address_id` int(11) DEFAULT NULL,
+  `purchaser_id` int(11) DEFAULT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `shipped` datetime DEFAULT NULL,
+  `payment_type` enum('PayPal','Glosspoints') DEFAULT NULL,
+  `price` int(11) DEFAULT NULL,
+  PRIMARY KEY (`order_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `products` (
+  `product_id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_name` varchar(55) DEFAULT NULL,
+  `price_cents` int(11) DEFAULT NULL,
+  `glosspoints_value` int(11) DEFAULT NULL,
+  `description` text,
+  `s3_thumbnail` varchar(55) DEFAULT NULL,
+  `s3_image` varchar(55) DEFAULT NULL,
+  PRIMARY KEY (`product_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `shipping_addresses`
+--
+
+DROP TABLE IF EXISTS `shipping_addresses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `shipping_addresses` (
+  `shipping_address_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `address1` varchar(255) DEFAULT NULL,
+  `address2` varchar(255) DEFAULT NULL,
+  `city` varchar(55) DEFAULT NULL,
+  `state` varchar(55) DEFAULT NULL,
+  `postal_code` varchar(15) DEFAULT NULL,
+  `country` varchar(55) DEFAULT 'United States',
+  `owner_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`shipping_address_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `shopping_cart_items`
+--
+
+DROP TABLE IF EXISTS `shopping_cart_items`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `shopping_cart_items` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_id` int(11) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `price_per_item` int(11) DEFAULT NULL,
+  `glosspoints_per_item` int(11) DEFAULT NULL,
+  `order_id` int(11) DEFAULT NULL,
+  `column_7` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `transaction_ledger`
+--
+
+DROP TABLE IF EXISTS `transaction_ledger`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `transaction_ledger` (
+  `txid` int(11) NOT NULL AUTO_INCREMENT,
+  `glosspoint_id` int(11) DEFAULT NULL,
+  `sender_id` int(11) DEFAULT NULL,
+  `receiver_id` int(11) DEFAULT NULL,
+  `initiated_by` int(11) DEFAULT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`txid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -81,9 +210,10 @@ CREATE TABLE `users` (
   `created` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `created_ip` varchar(15) DEFAULT NULL,
   `session_token` char(16) DEFAULT NULL,
+  `json_metadata` text,
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `email_address` (`email_address`)
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -95,4 +225,4 @@ CREATE TABLE `users` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-08-23 21:42:31
+-- Dump completed on 2018-10-02  3:29:19
