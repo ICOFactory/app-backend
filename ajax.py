239149b7096e02e82b8d1ae027b8f8cdbbb15a97
John Hill <xs.coating@gmail.com>
2019-02-05 04:47:30 -0600

    bug fixes

--- ajax.py
+++ ajax.py
@@ -1,10 +1,8 @@
 from flask import (
-    Blueprint, request, Response, current_app
+    Blueprint, Response, current_app
 )
-from werkzeug.exceptions import abort
 import json
-from events import Event
-from block_data import BlockDataManager, BlockData
+from block_data import BlockDataManager
 
 ajax_api_blueprint = Blueprint('ajax', __name__, url_prefix="/ajax")
 
@@ -13,18 +11,24 @@ ajax_api_blueprint = Blueprint('ajax', __name__, url_prefix="/ajax")
 def update_block_data(latest_block):
     manager = BlockDataManager(logger=current_app.logger)
     new_block_numbers = manager.get_block_numbers_since(latest_block)
-    block_data = []
+    new_block_data = []
     for each_block_number in new_block_numbers:
         block = manager.get_block_from_db(each_block_number, True)
+        # convert to JSON serializable/JavaScript format
+        block_timestamp = block["block_timestamp"]
+        block["block_timestamp"] = block_timestamp.isoformat()
         if block:
-            block_data.append(block)
-    return Response(json.dumps(block_data), content_type="application/json")
+            new_block_data.append(block)
+    return Response(json.dumps(new_block_data), content_type="application/json")
 
 
 @ajax_api_blueprint.route("/block_data/<block_number>")
 def block_data(block_number):
     manager = BlockDataManager(logger=current_app.logger)
     data = manager.get_block_from_db(block_number)
+    # convert to JSON serializable/JavaScript format
+    block_timestamp = data["block_timestamp"]
+    data["block_timestamp"] = block_timestamp.isoformat()
     if data:
         return Response(json.dumps(data), content_type="application/json")
     else:
