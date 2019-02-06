711273a3476e86441972f4799264321304d4db68
John Hill <xs.coating@gmail.com>
2019-02-05 21:12:56 -0600

    misspellings

--- static/block_explorer.js
+++ static/block_explorer.js
@@ -25,10 +25,10 @@ $(document).ready(function () {
                     let gwei = transactions[tx].priority / 1000000000;
                     var difference = null
                     if (result["moving_average_gas_price"] < transactions[tx].priority) {
-                        difference = "<span style=\"color:rgb(0,183,79)\">(-" + (result["moving_average_gas_price"] - transactions[tx].priority) / 1000000000 + " Gwei)</span>"
+                        difference = "<span style=\"color:rgb(0,183,79)\">(-" + (result["moving_average_gas_price"] - transactions[tx].priority) / 1000000000 + " Gwei)</span>";
                     }
                     else {
-                        differnece = "<span style=\"color:rgb(209,103,90)\">" + (transactions[tx].priority - result["moving_average_gas_price"])/ 1000000000 + " Gwei)</span>"
+                        difference = "<span style=\"color:rgb(209,103,90)\">" + (transactions[tx].priority - result["moving_average_gas_price"])/ 1000000000 + " Gwei)</span>";
                     }
                     $table.append("<tr class=\"ui_div_highlight\"><td colspan='2'><span class=\"tx_details_label\">" + transactions[tx].hash + "</span></td></tr>");
                     $table.append("<tr><td><span class=\"tx_details_label\">From</span></td><td align='right'><span class=\"tx_details_value\">" + transactions[tx].from + "</span></td></tr>");
